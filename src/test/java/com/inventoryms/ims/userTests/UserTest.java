ebe5fec37b03e51773703db109eeffcb8a341891
mwala-zm <mwalamulenga.zt@gmail.com>
2023-06-07 11:29:42 +0200

    add new user test

--- src/test/java/com/inventoryms/ims/userTests/UserTest.java
+++ src/test/java/com/inventoryms/ims/userTests/UserTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import com.inventoryms.ims.models.Role;
 import com.inventoryms.ims.models.User;
@@ -26,20 +27,37 @@ public class UserTest {
     @Test
     public void testRoleAssignment() {
 
-    Role role = new Role(Role.STOCK_CLERK);
-    Role savedRole = roleRepository.save(role);
+        Role role = new Role(Role.STOCK_CLERK);
+        Role savedRole = roleRepository.save(role);
 
-    // Create a user
-    User user = new User("John Doe", role);
-    user.setRole(savedRole);
+        // Create a user
+        User user = new User("Basswell", role);
+        user.setRole(savedRole);
 
+        userRepository.save(user);
 
-    userRepository.save(user);
+        User savedUser = userRepository.findById(user.getId()).orElse(null);
 
-    // Retrieve the user from the repository
-    User savedUser = userRepository.findById(user.getId()).orElse(null);
+        // Test the role assignment
+        assertEquals(Role.STOCK_CLERK, savedUser.getRole().getName());
+    }
 
-    // Test the role assignment
-    assertEquals(Role.STOCK_CLERK, savedUser.getRole().getName());
+    @Test
+    public void createUser() {
+        Role role = new Role(Role.RECEIVING_CLERK);
+        User user = new User("Joshua", role);
+
+        assertEquals("Joshua", user.getName());
+        assertEquals(role, user.getRole());
+    }
+
+    @Test
+    public void testUserEquality() {
+        Role role1 = new Role(Role.SHIPPING_CLERK);
+        Role role2 = new Role(Role.RECEIVING_CLERK);
+        User user1 = new User("Mwala", role1);
+        User user2 = new User("Ziwelanji", role2);
+
+        assertNotEquals(user1, user2);
     }
 }
